4b353de71a85452c5ed00e2fd115771e142069f6
Christian Jánský <chrisjansky@gmail.com>
2015-10-09 20:33:40 +0200

    JS Cleaned up app.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,33 +1,38 @@
 var
+  vHeight = window.innerHeight,
   $body = $("body"),
-  $header = $("[data-header]"),
   $stamp = $("#js-stamp");
 
 var
-  // projectsInview = $("[data-projects]").waypoint({
-  //   handler: function(direction) {
-  //     $body.toggleClass("scrolled--up", direction === "up");
-  //     $body.toggleClass("scrolled--down", direction === "down");
-  //   },
-  //   offset: "50%"
-  // }),
   sectionInview = $("[data-section]").waypoint({
     handler: function(direction) {
       var
+        slug = $(this.element).data("slug"),
         $target = direction === "down" ? $(this.element) : $(this.element).prevAll("[data-section]:first");
-        // $prev = $(this.element).prev().length > 0 ? $(this.element).prev() : $("[data-projects]");
-
-      // No previous object
-      // if ($target.length < 1) {
-      //   $target = $("[data-projects]");
-      // }
 
       $stamp.text($target.data("stamp"));
 
       $body.toggleClass("section--" + $(this.element).prevAll("[data-section]:first").data("slug"), direction === "up");
-      $body.toggleClass("section--" + $(this.element).data("slug"), direction === "down");
+      $body.toggleClass("section--" + slug, direction === "down");
+
+      if (slug === "colours") {
+        $body.toggleClass("body--scrolled", direction === "down");
+      }
 
     },
     // Top of object gets into center of viewport.
     offset: "50%"
+  }),
+  introInview = $("[data-intro]").waypoint({
+    handler: function(direction) {
+      $body.toggleClass("body--scrolled", direction === "down");
+    }
   });
+
+$(window).on("resize orientationchange", function() {
+  clearTimeout(this.id)
+  
+  this.id = setTimeout(function() {
+    vHeight = window.innerHeight;
+  }, durBasic)
+});
